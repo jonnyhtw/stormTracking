0fc4d4271bf7e303ad6dacdb37bddd86cb398b54
Jonny Williams <jonny.williams@niwa.co.nz>
2021-05-03 05:05:31 +0000

    nzesm4

--- storm_detection.py
+++ storm_detection.py
@@ -32,23 +32,24 @@ print('startyear = ', startyear)
 model_data = True
 
 if model_data:
-    dataset = 'u-bc179'
+    dataset = 'u-bw947'
 else:
     dataset = 'NCEP_20CRV2C'
 
 # Parameters
-pathroot = {'NCEP_20CRV2C': '/nesi/project/niwa00013/williamsjh/NZESM/storm/data/NCEP/20CRv2c/prmsl/6hourly/', 'u-bl658': '/nesi/project/niwa00013/williamsjh/NZESM/storm/model-data/u-bl658/','u-bb075': '/nesi/project/niwa00013/williamsjh/MASS/u-bb075/apc.pp/m01s16i222/','u-bd483': '/nesi/project/niwa00013/williamsjh/MASS/u-bd483/apc.pp/m01s16i222/','u-bf656': '/nesi/project/niwa00013/williamsjh/MASS/u-bf656/apc.pp/m01s16i222/','u-bc179': '/nesi/project/niwa00013/williamsjh/MASS/u-bc179/apc.pp/m01s16i222/','u-bo721': '/nesi/project/niwa00013/williamsjh/NZESM/storm/model-data/u-bo721/','u-bx226': '/nesi/project/niwa00013/williamsjh/NZESM/storm/model-data/u-bx226/'}
-var = {'NCEP_20CRV2C': 'prmsl', 'u-bl658': 'air_pressure_at_sea_level', 'u-bb075': 'air_pressure_at_sea_level', 'u-bd483': 'air_pressure_at_sea_level', 'u-bf656': 'air_pressure_at_sea_level',  'u-bo721': 'air_pressure_at_sea_level',  'u-bx226': 'air_pressure_at_sea_level',  'u-bc179': 'air_pressure_at_sea_level'}
+pathroot = {'NCEP_20CRV2C': '/nesi/project/niwa00013/williamsjh/NZESM/storm/data/NCEP/20CRv2c/prmsl/6hourly/', 'u-bl658': '/nesi/project/niwa00013/williamsjh/NZESM/storm/model-data/u-bl658/','u-bb075': '/nesi/project/niwa00013/williamsjh/MASS/u-bb075/apc.pp/m01s16i222/','u-bd483': '/nesi/project/niwa00013/williamsjh/MASS/u-bd483/apc.pp/m01s16i222/','u-bf656': '/nesi/project/niwa00013/williamsjh/MASS/u-bf656/apc.pp/m01s16i222/','u-bc179': '/nesi/project/niwa00013/williamsjh/MASS/u-bc179/apc.pp/m01s16i222/','u-bo721': '/nesi/project/niwa00013/williamsjh/NZESM/storm/model-data/u-bo721/','u-bx226': '/nesi/project/niwa00013/williamsjh/NZESM/storm/model-data/u-bx226/','u-bw947': '/nesi/project/niwa00013/williamsjh/NZESM/storm/model-data/u-bw947/'}
+var = {'NCEP_20CRV2C': 'prmsl', 'u-bl658': 'air_pressure_at_sea_level', 'u-bb075': 'air_pressure_at_sea_level', 'u-bd483': 'air_pressure_at_sea_level', 'u-bf656': 'air_pressure_at_sea_level',  'u-bo721': 'air_pressure_at_sea_level',  'u-bx226': 'air_pressure_at_sea_level',  'u-bc179': 'air_pressure_at_sea_level',  'u-bx226': 'air_pressure_at_sea_level',  'u-bc179': 'air_pressure_at_sea_level',  'u-bw947': 'air_pressure_at_sea_level'}
 
 # Generate date and hour vectors
-yearStart = {'NCEP_20CRV2C': startyear,  'u-bl658': startyear, 'u-bb075': startyear, 'u-bd483': startyear, 'u-bf656': startyear, 'u-bo721': startyear, 'u-bx226': startyear, 'u-bc179': startyear}
-yearEnd = {'NCEP_20CRV2C': startyear,  'u-bl658': startyear, 'u-bb075': startyear, 'u-bd483': startyear, 'u-bf656': startyear, 'u-bo721': startyear, 'u-bx226': startyear, 'u-bc179': startyear}
+yearStart = {'NCEP_20CRV2C': startyear,  'u-bl658': startyear, 'u-bb075': startyear, 'u-bd483': startyear, 'u-bf656': startyear, 'u-bo721': startyear, 'u-bx226': startyear, 'u-bc179': startyear, 'u-bw947': startyear}
+yearEnd = {'NCEP_20CRV2C': startyear,  'u-bl658': startyear, 'u-bb075': startyear, 'u-bd483': startyear, 'u-bf656': startyear, 'u-bo721': startyear, 'u-bx226': startyear, 'u-bc179': startyear, 'u-bw947': startyear}
 
 # Load lat, lon
 filename = {'NCEP_20CRV2C': pathroot['NCEP_20CRV2C'] + 'prmsl.' + str(yearStart['NCEP_20CRV2C']) + '.nc',
             'u-bl658': pathroot['u-bl658'] + 'regrid-bl658a.pc' + str(yearStart['u-bl658']) + '.nc', 
             'u-bo721': pathroot['u-bo721'] + 'regrid-bo721a.pc' + str(yearStart['u-bo721']) + '.nc',
             'u-bx226': pathroot['u-bx226'] + 'regrid-bx226a.pc' + str(yearStart['u-bx226']) + '.nc',
+            'u-bw947': pathroot['u-bw947'] + 'regrid-bw947a.pc' + str(yearStart['u-bw947']) + '.nc',
             'u-bb075': pathroot['u-bb075'] + 'regrid-bb075a.pc' + str(yearStart['u-bb075']) + '.nc',
             'u-bf656': pathroot['u-bf656'] + 'regrid-bf656a.pc' + str(yearStart['u-bf656']) + '.nc',
             'u-bc179': pathroot['u-bc179'] + 'regrid-bc179a.pc' + str(yearStart['u-bc179']) + '.nc',
@@ -71,6 +72,7 @@ for yr in range(yearStart[dataset], yearEnd[dataset]+1):
         filename = {'u-bl658': pathroot['u-bl658'] + 'regrid-bl658a.pc' + str(yearStart['u-bl658']) + '.nc',
                     'u-bo721': pathroot['u-bo721'] + 'regrid-bo721a.pc' + str(yearStart['u-bo721']) + '.nc',
                     'u-bx226': pathroot['u-bx226'] + 'regrid-bx226a.pc' + str(yearStart['u-bx226']) + '.nc',
+                    'u-bw947': pathroot['u-bw947'] + 'regrid-bw947a.pc' + str(yearStart['u-bw947']) + '.nc',
                     'u-bb075': pathroot['u-bb075'] + 'regrid-bb075a.pc' + str(yearStart['u-bb075']) + '.nc',
                     'u-bf656': pathroot['u-bf656'] + 'regrid-bf656a.pc' + str(yearStart['u-bf656']) + '.nc',
                     'u-bc179': pathroot['u-bc179'] + 'regrid-bc179a.pc' + str(yearStart['u-bc179']) + '.nc',
